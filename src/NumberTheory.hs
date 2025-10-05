574bb341f480b6a06e68b271b6c46508215426c9
Silver <tealsilver1121@gmail.com>
2025-10-05 18:52:14 -0400

    added stuff from number theory class and extra functions

--- src/NumberTheory.hs
+++ src/NumberTheory.hs
@@ -58,7 +58,7 @@ primePowerFactors :: (Integral a, Num b, Eq b) => a -> [(b, a)]
 primePowerFactors n = ppf (0, 0) (primeFactors n)
     where
     ppf (l, e) [] = [(l, e)]
-    ppf (l, e) (p:ps) 
+    ppf (l, e) (p:ps)
         | l == p = ppf (l, e + 1) ps
         | l == 0 = ppf (p, 1) ps
         | otherwise = (l, e) : ppf (p, 1) ps
@@ -81,6 +81,14 @@ is2prime n = even n && isPrime (n `div` 2)
 
 isPrimePower :: Integral a => a -> Bool
 isPrimePower n = n >= 2 && all (== head (primeFactors n)) (primeFactors n)
+
+-- for some positive integers a and b, finds some integers x and y s.t. ax + by = gcd(a,b), returns x, y, gcd(a,b)
+extendGCD :: (Integral t, Num a, Num b, Num c) => t -> t -> (a, b, c)
+extendGCD a b = if b > a then egcd b a (b `div` a) 0 1 1 0 else egcd a b (a `div` b) 1 0 0 1
+    where
+    egcd c d q x1 y1 x2 y2
+        | d == 0    = (fromIntegral x1, fromIntegral y1, fromIntegral c)
+        | otherwise = egcd d (c - q * d) (d `div` (c - q * d)) x2 y2 (x1 - (q * x2)) (y1 - (q * y2))
 
 -- order of a mod p, or the smallest positive exponent e s.t. a^e === 1 mod p
 -- p must be a prime
@@ -116,13 +124,20 @@ mobius n = if littleOmega n == bigOmega n then liouville n else 0
 mertens :: (Integral a, Num b) => a -> b
 mertens n = sum $ map mobius [1..n]
 
+-- Dedekind psi function 
+dedekind :: (Integral a, Num b) => a -> b
+dedekind n
+    | n < 1     = undefined
+    | n == 1    = 1
+    | otherwise = fromIntegral $ product $ map (\(p, k) -> (p + 1) * (p ^ (k - 1))) (primePowerFactors n)
+
 -- Euler's totient function, counting the number of relative primes of n up to n
 totient :: (Integral a, Num b) => a -> b
 totient 1 = 1
-totient n 
+totient n
     | n < 1 = undefined
     | otherwise = fromIntegral $ product $ map totient' (primePowerFactors n)
-    where 
+    where
     totient' (p, k)
         | k == 1    = p - 1
         | p == 2    = p ^ (k - 1)
@@ -130,6 +145,29 @@ totient n
 
 cototient :: (Integral a, Num b) => a -> b
 cototient n = fromIntegral n - totient n
+
+-- finds all numbers n s.t. phi(n) = m
+inverseTotient :: (Integral a, Num b) => a -> [b]
+inverseTotient m
+    | m < 1     = []
+    | m == 1    = [1, 2]
+    | odd m     = []
+    | otherwise = [fromIntegral n | n <- [(m + 1)..(2 * (m ^ 2))], m == totient n]
+
+-- Dirichlet inverse of phi 
+dirInvTotient :: (Integral a, Num b) => a -> b
+dirInvTotient n = fromIntegral $ sum [d * mobius d | d <- factors n]
+
+carmichael :: (Integral a, Num b) => a -> b
+carmichael n
+    | n < 1 = undefined
+    | n == 1 = 1
+    | otherwise = fromIntegral $ foldr (lcm . carmichael') 1 (primePowerFactors n)
+    where
+    carmichael' (p, k)
+        | k == 1    = p - 1
+        | p == 2    = p ^ (k - 2)
+        | otherwise = (p - 1) * (p ^ (k - 1))
 
 -- sum of proper divisors, only works on strictly positive integers
 aliquotSum :: (Integral a, Num b) => a -> b
@@ -144,6 +182,12 @@ divisorSum n = if n > 0 then (sum . factors) n else undefined
 
 divisorCount :: (Integral a, Num b) => a -> b
 divisorCount = fromIntegral . length . factors
+
+isSquareFree :: Integral a => a -> Bool
+isSquareFree = unique 0 . primeFactors
+    where
+    unique _ [] = True
+    unique l (x:xs) = (l /= x) && unique x xs
 
 isDeficient :: Integral a => a -> Bool
 isDeficient n = n > aliquotSum n
@@ -180,12 +224,20 @@ isSemiperfect n = subsetSum n (length pfn - 1)
         | k < 0 || l < 0 = False
         | otherwise      = subsetSum k (l - 1) || subsetSum (k - (pfn !! l)) (l - 1)
 
---isWeird
+isPerfectTotient :: Integral a => a -> Bool
+isPerfectTotient n = n == ipt n
+    where
+    ipt 1 = 0
+    ipt k = totient k + ipt (totient k)
+
+-- might want to make this more efficient
+isWeird :: Integral a => a -> Bool
+isWeird n = isAbundant n && not (isSemiperfect n)
 
 isHighlyComposite :: Integral a => a -> Bool
 isHighlyComposite n = all ((< divisorCount n) . divisorCount) [1..n - 1]
 
---isSuperiorHighlyComposite
+--isSuperiorHighlyComposite :: Integral a => a -> Bool -- this is hard :(
 
 isPowerful :: Integral a => a -> Bool
 isPowerful n = dupCheck $ primeFactors n
@@ -199,3 +251,7 @@ isPowerful n = dupCheck $ primeFactors n
 -- prime factors of n are less than or equal to b
 isBsmooth :: Integral a => a -> a -> Bool
 isBsmooth b n = b >= last (distinctPrimeFactors n)
+
+-- generates the nth primitive pythagorean triple via (a,b,c) = (u^2 - v^2, 2uv, u^2 + v^2) for some integers u,v
+pythagTriple :: (Num a, Num b, Num c) => Int -> (a, b, c)
+pythagTriple n = [(fromIntegral ((u ^ 2) - (v ^ 2)), fromIntegral (2 * u * v), fromIntegral ((u ^ 2) + (v ^ 2))) | u <- [2..], v <- [1..u - 1], odd (u + v)] !! (n - 1)
